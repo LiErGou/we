71738ed141ff77f65b67c0c65ac28619aedc4f8f
LiErGou <lichangliangsky@126.com>
2017-09-30 12:42:01 +0800

    9.30
    9.30

---
+++ CoolWeather/app/src/main/java/com/example/administrator/coolweather/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.administrator.coolweather;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getString("weather",null)!=null){
+            Intent intent=new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
+}
---
+++ CoolWeather/app/src/main/java/com/example/administrator/coolweather/db/City.java
@@ -0,0 +1,45 @@
+package com.example.administrator.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/9/25.
+ */
+public class City extends DataSupport {
+    private int id;
+    private String CityName;
+    private int cityCode;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String cityName) {
+        CityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
